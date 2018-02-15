c9cd2d13ae5b5adb7d05f8f7270a24c23119478c
Prasad Honrao <Honrao.Prasad@hotmail.com>
2018-02-15 17:13:33 +0530

    Integrate login page

---
+++ crispyspoon/crispyspoon/crispyspoon/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crispyspoon.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+            Title = "Login";
+        }
+    }
+}
--- crispyspoon/crispyspoon/crispyspoon/Views/MainPage.xaml.cs
+++ crispyspoon/crispyspoon/crispyspoon/Views/MainPage.xaml.cs
@@ -6,11 +6,18 @@ using Xamarin.Forms.Xaml;
 namespace crispyspoon.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage
-	{
+	public partial class MainPage : ContentPage
+    {
 		public MainPage ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        async void OnLogout_Clicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+    }
 }
